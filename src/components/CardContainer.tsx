2ee0d325d9a9fab499c47ed0604b940ef6d507ac
Artur J <artur.jedrzejczak@gmail.com>
2023-03-16 12:31:05 +0100

    ads hooks to inputs

--- src/components/CardContainer.tsx
+++
@@ -1,7 +0,0 @@
-import React, { PropsWithChildren } from 'react';
-
-const Card = ({ children }: PropsWithChildren) => (
-  <section className="card box_shadow">{children}</section>
-);
-
-export default Card;
